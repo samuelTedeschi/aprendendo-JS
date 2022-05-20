04ed63ec0d23aa6f12733de7caa55feed53d8ca2
samuel tedeschi <samueltedeschi100@yahoo.com>
2022-05-20 16:35:21 -0300

    atualizado

---
+++ 1_Primeiro_Programa/script.js
@@ -0,0 +1,26 @@
+
+// primeiro variavel
+
+// variavel com var 
+var nome = 'samuel'
+var nomeSobrenome = "samuel tedeschi"
+
+console.log(nome)
+console.log(nomeSobrenome)
+
+// variavel com let
+let idade = 30
+console.log(idade)
+
+// variavel com const
+const genero = 'masculino'
+console.log(genero)
+
+// este é um comentari 
+
+/*  comentario
+    de
+    multiplas
+    linhas
+
+*/

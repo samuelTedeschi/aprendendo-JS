975e1046d79e06d8422e0033dbcdd3ea601dca6d
samuel tedeschi <samueltedeschi100@yahoo.com>
2022-05-20 20:32:50 -0300

    atualizado

--- 1_Primeiro_Programa/script.js
+++ 1_Primeiro_Programa/script.js
@@ -1,26 +1,38 @@
-
-// primeiro variavel
-
-// variavel com var 
-var nome = 'samuel'
-var nomeSobrenome = "samuel tedeschi"
-
-console.log(nome)
-console.log(nomeSobrenome)
-
-// variavel com let
-let idade = 30
-console.log(idade)
-
-// variavel com const
-const genero = 'masculino'
-console.log(genero)
-
-// este é um comentari 
+// este é um comentari o
 
 /*  comentario
     de
     multiplas
     linhas
-
 */
+
+document.write("<h1 style='color:blue'>bem vindo! <br> Introduçao a JavaScript</h1>")
+
+// variavel 
+
+var nome = "fulano"
+var idade = "20 anos "
+
+document.write(nome + "<br>" + idade + "<br>")  
+
+var carro = "gol "
+var ano = 2013
+var preco = 2000
+var desconto = 500 
+
+var comprar = carro + ano + "<br>" + (preco - desconto); 
+
+document.write(comprar)
+
+// costante 
+
+const unico = "costante nao muda seu seu valor como a variavel"
+
+document.write("<br>" + unico)
+
+// alert
+
+var saldaçao = "bem vindo";
+const num = 20;
+
+alert(saldaçao + " " + num)

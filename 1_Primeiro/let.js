37177bc710b462fbfed464e650a747c28c9b8110
samuel tedeschi <samueltedeschi100@yahoo.com>
2022-05-26 21:46:18 -0300

    add

---
+++ 1_Primeiro/let.js
@@ -0,0 +1,9 @@
+
+    var num=10;
+
+    if(num==10){
+        let num=5;
+        document.write(num+"<br>")
+    }
+
+    document.write(num)
---
+++ 1_Primeiro/while.js
@@ -0,0 +1,47 @@
+
+    /* 
+    var num;
+
+    num=0;
+
+    while(num<10){
+        document.write(num+"<br>");
+        num++;
+
+    }
+
+    document.write("<hr>")
+
+    num=10
+    while(num--){
+        document.write(num+"<br>")
+    }
+
+    document.write("<hr>")
+
+    num=true;
+    cont=0;
+    while(num){
+        document.write("Curso Js <br>")
+        cont++;
+        if(cont>9){
+            num=false;
+        }
+    }
+*/
+
+// do while 
+
+var num = 15;
+
+/*  while(num<10){
+        document.write("javascript <br>")
+        num++;
+}   
+*/
+
+do{
+    document.write("javascript <br>")
+    num++;
+
+}while(num<10);

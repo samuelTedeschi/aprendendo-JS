2b24ede702ffb2f07d6c60817a7ae4da6f54a608
samuel tedeschi <samueltedeschi100@yahoo.com>
2022-06-23 15:39:50 -0300

    atualizado

--- 5-jogo-da-velha/script.js
+++ 5-jogo-da-velha/script.js
@@ -16,6 +16,72 @@ function jogar (p){
                     quemJoga=1;
                 }
             break;
+            case 2:
+                if(jogo[0][1]==""){
+                    jogo[0][1]="x";
+                    quemJoga=1;
+                }
+            break;
+            case 3:
+                if(jogo[0][2]==""){
+                    jogo[0][2]="x";
+                    quemJoga=1;
+                }
+            break;
+            case 4:
+                if(jogo[1][0]==""){
+                    jogo[1][0]="x";
+                    quemJoga=1;
+                }
+            break;
+            case 5:
+                if(jogo[1][1]==""){
+                    jogo[1][1]="x";
+                    quemJoga=1;
+                }
+            break;
+            case 6:
+                if(jogo[1][2]==""){
+                    jogo[1][2]="x";
+                    quemJoga=1;
+                }
+            break;
+            case 7:
+                if(jogo[2][0]==""){
+                    jogo[2][0]="x";
+                    quemJoga=1;
+                }
+            break;
+            case 8:
+                if(jogo[2][1]==""){
+                    jogo[2][1]="x";
+                    quemJoga=1;
+                }
+            break;
+            default:
+                if(jogo[2][2]==""){
+                    jogo[2][2]="x";
+                    quemJoga=1;
+                }
+            break;
+        }
+        atualizaTabuleiro();
+    }
+}
+
+function atualizaTabuleiro(){
+    for (var l=0;l<3;l++){
+        for (var c=0;c<3;c++){
+            if(jogo[l][c]=="X"){
+                tabuleiro[l][c].innerHTML="X";
+                tabuleiro[l][c].style.cursor="default";
+            }else if(jogo[l][c]=="O"){
+                tabuleiro[l][c].innerHTML="O"
+                tabuleiro[l][c].style.cursor="default";
+            }else{
+                tabuleiro[l][c].innerHTML="";
+                tabuleiro[l][c].style.cursor="pointer";
+            }
         }
     }
 }
@@ -28,6 +94,12 @@ function iniciar(){
         ["","",""],
         ["","",""]
     ];
+    tabuleiro=[
+        [document.getElementById("p1"),document.getElementById("p2"),document.getElementById("p3")]
+        [document.getElementById("p4"),document.getElementById("p5"),document.getElementById("p6")]
+        [document.getElementById("p7"),document.getElementById("p8"),document.getElementById("p9")]
+    ];
+
 }
 
 window.addEventListener("load",iniciar);

694d2c747afec72a4420af9c8165d6a7683aa5f8
samuel tedeschi <samueltedeschi100@yahoo.com>
2022-06-24 17:53:24 -0300

    jogo da velha atualizado

--- 5-jogo-da-velha/script.js
+++ 5-jogo-da-velha/script.js
@@ -1,13 +1,35 @@
 var jogo=[];
 var tabuleiro=[];
 var quemJoga=0; //0-jogador 1-cpu
-var verificar;
+var verifica;
 var jogando=true;
 var nivel=1;
 var jogadaCpu=1;
 var quemComeca=1;
 
-function jogar (p){
+function cpu(){
+    if(jogando){
+        var l,c;
+        if(nivel==1){
+            do{
+                l = Math.round(Math.random()*2);
+                c = Math.round(Math.random()*2);
+            }while(jogo[l][c]!="");
+            jogo[l][c]="O";
+        }else if(nivel==2){
+
+        }
+        verifica = verificaVitoria();
+        if (verifica!=""){
+            alert(verifica+" win ");
+            jogando=false;
+        }
+        atualizaTabuleiro();
+        quemJoga=0;
+    }
+}
+
+function jogar(p){
     if ((jogando)&&(quemJoga==0)){
         switch(p){
             case 1:
@@ -64,29 +86,63 @@ function jogar (p){
                     quemJoga=1;
                 }
             break;
+        };
+        if(quemJoga==1){
+            atualizaTabuleiro();
+            verifica = verificaVitoria();
+            if (verifica!=""){
+                alert(verifica + " win ");
+                jogando=false;
+            }
+            cpu();
+        };
+    };
+
+};
+
+function verificaVitoria(){
+    var l,c;
+    //linha
+    for(l=0;l<3;l++){
+        if((jogo[l][0]==jogo[l][1])&&(jogo[l][1]==jogo[l][2])){
+            return jogo [l][0];
         }
-        atualizaTabuleiro();
     }
+    //coluna
+    for(c=0;c<3;c++){
+        if((jogo[0][c]==jogo[1][c])&&(jogo[1][c]==jogo[2][c])){
+            return jogo [0][c];
+        }
+    }
+    //diagonal
+    if((jogo[0][0]==jogo[1][1])&&(jogo[1][1]==jogo[2][2])){
+        return jogo [0][0];
+    }
+    if((jogo[0][2]==jogo[1][1])&&(jogo[1][1]==jogo[2][0])){
+        return jogo [0][2];
+    };
+    return "";
+
 }
 
 function atualizaTabuleiro(){
-    for (var l=0;l<3;l++){
-        for (var c=0;c<3;c++){
-            if(jogo[l][c]=="X"){
-                tabuleiro[l][c].innerHTML="X";
+    for(var l=0;l<3;l++){
+        for(var c=0;c<3;c++){
+            if(jogo[l][c]=="x"){
+                tabuleiro[l][c].innerHTML="x";
                 tabuleiro[l][c].style.cursor="default";
             }else if(jogo[l][c]=="O"){
-                tabuleiro[l][c].innerHTML="O"
+                tabuleiro[l][c].innerHTML="O";
                 tabuleiro[l][c].style.cursor="default";
             }else{
                 tabuleiro[l][c].innerHTML="";
                 tabuleiro[l][c].style.cursor="pointer";
-            }
-        }
-    }
-}
+            };
+        };
+    };
+}; 
 
-function iniciar(){
+function inicia(){
     jogando=true;
     jogadaCpu=1;
     jogo=[
@@ -95,11 +151,11 @@ function iniciar(){
         ["","",""]
     ];
     tabuleiro=[
-        [document.getElementById("p1"),document.getElementById("p2"),document.getElementById("p3")]
-        [document.getElementById("p4"),document.getElementById("p5"),document.getElementById("p6")]
+        [document.getElementById("p1"),document.getElementById("p2"),document.getElementById("p3")],
+        [document.getElementById("p4"),document.getElementById("p5"),document.getElementById("p6")],
         [document.getElementById("p7"),document.getElementById("p8"),document.getElementById("p9")]
     ];
 
 }
 
-window.addEventListener("load",iniciar);
+window.addEventListener("load",inicia);
